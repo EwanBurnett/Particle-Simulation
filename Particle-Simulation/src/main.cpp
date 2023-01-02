545615d67e220f21d471ebf41de6b0c2f981619e
Ewan Burnett <EwanBurnettSK@outlook.com>
2023-01-02 02:31:40 +0000

    Base Project Files
    Set up a basic C++ project. So far, no libraries have been linked (for
    graphics, etc.,).

---
+++ Particle-Simulation/src/main.cpp
@@ -0,0 +1,12 @@
+//Particle Simulation
+//Ewan Burnett - 2023
+//
+//A Simple 3D Particle Simulation, intended to practice C++ concurrency and parallelism techniques.
+
+#include <cstdio>	//For Logging
+
+int main()
+{
+	printf("Hello, World!");
+	return 0;
+}
